a8e23d4096e256dcd3b7d2f98d0d199a37ca1fef
funsi <topy@vip.qq.com>
2026-06-03 14:43:35 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <>
       <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">MIT LICENSE</a>
       <span>
-        <span>NewsSpace &copy; <span id="currentYearMonth"></span> Maintained by </span>
+        <span>NewsSpace &copy; 2026 Maintained by </span>
         <a href="https://funsi.com" target="_blank">Phi</a>
       </span>
     </>
